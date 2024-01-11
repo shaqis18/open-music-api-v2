64a9884fbfd6c3b26a3d3bc10617e3da1cf2ebf9
Arif Setiadi <arifasussetia89@gmail.com>
2024-01-11 08:03:28 +0700

    resolve playlist schema

--- src/validator/playlists/schema.js
+++ src/validator/playlists/schema.js
@@ -2,13 +2,20 @@ const Joi = require('joi');
 
 const PlaylistPayloadSchema = Joi.object({
   name: Joi.string().required(),
+  owner: Joi.string().required(),
 });
 
-const PlaylistSongPayloadSchema = Joi.object({
+const addPlaylistSongPayloadSchema = Joi.object({
+  playlistId: Joi.string().required(),
+  songId: Joi.string().required(),
+});
+
+const deletePlaylistSongPayloadSchema = Joi.object({
   songId: Joi.string().required(),
 });
 
 module.exports = {
   PlaylistPayloadSchema,
-  PlaylistSongPayloadSchema,
+  addPlaylistSongPayloadSchema,
+  deletePlaylistSongPayloadSchema,
 };
