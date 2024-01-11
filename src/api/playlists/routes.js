68be109751f9094a4e6b2424c7a60ed116717d0f
Arif Setiadi <arifasussetia89@gmail.com>
2024-01-11 12:16:50 +0700

    test

--- src/api/playlists/routes.js
+++ src/api/playlists/routes.js
@@ -3,16 +3,49 @@ const routes = (handler) => [
     method: 'POST',
     path: '/playlists',
     handler: handler.postPlaylistHandler,
+    Options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+  {
+    method: 'POST',
+    path: '/playlists/{id}/songs',
+    handler: handler.postPlaylistSongHandler,
+    Options: {
+      auth: 'openmusic_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/playlists',
     handler: handler.getPlaylistsHandler,
+    Options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+  {
+    method: 'GET',
+    path: '/playlists/{id}/songs',
+    handler: handler.getPlaylistSongsHandler,
+    Options: {
+      auth: 'openmusic_jwt',
+    },
   },
   {
     method: 'DELETE',
     path: '/playlists/{id}',
     handler: handler.deletePlaylistByIdHandler,
+    Options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/playlists/{id}/songs',
+    handler: handler.deletePlaylistSongByIdHandler,
+    Options: {
+      auth: 'openmusic_jwt',
+    },
   },
 ];
 
